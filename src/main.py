9a4e21ab0cab28b98ccde358f3cf9bf1ce96938a
Liam-M-Dev <liammassey21@gmail.com>
2023-03-03 17:57:59 +1100

    Initialize basic configuration, Initalize create_app function

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+def create_app():
+
+    app = Flask(__name__)
+
+    app.config.from_object("config.app_config")
+
+    db = SQLAlchemy(app)
+
+    return app
