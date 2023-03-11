a45a2bd4adb64da5d61d101dff23a00f16da4576
Liam-M-Dev <liammassey21@gmail.com>
2023-03-11 19:40:25 +1100

    Create venue update route

--- src/controllers/venue_controller.py
+++ src/controllers/venue_controller.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, jsonify, request, abort
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from decorators import error_handlers
 from main import db
 from models.venue import Venue
+from models.user import User
 from models.show import Show
 from schemas.venue_schema import venue_schema, venues_schema, VenueSchema
 from schemas.show_schema import show_schema, ShowSchema
@@ -31,12 +34,73 @@ def display_venue(id):
 
     venue = db.get_or_404(Venue, id, description="Invalid venue id, please check ID")
 
-    venue_schema = VenueSchema(only=["id", "venue_name", "location"])
+    venue_display = VenueSchema(only=["id", "venue_name", "location"])
 
     upcoming_shows = db.get_or_404(Show, venue.id, description="There are no upcoming shows")
 
-    show_schema = ShowSchema(only=["show_name", "date"])
+    show_display = ShowSchema(only=["show_name", "date"])
+
+    return jsonify(venue_display.dump(venue), show_display.dump(upcoming_shows))
+
+
+# Post method to allow user to create a new venue
+# method requires user id to authenticate user
+# method takes data input via fields for venue name, location
+# sets venue user id to the users id
+# returns json object of venue data
+@venues.route("/register/<int:id>", methods=["POST"])
+@jwt_required()
+@error_handlers
+def band_creation(id):
+
+    user = get_jwt_identity()
+
+    user = db.get_or_404(User, id, description="Invalid user, please check id")
+
+
+
+    venue_fields = venue_schema.load(request.json)
+
+    venue = Venue.query.filter_by(venue_name=venue_fields["venue_name"]).first()
+    if venue:
+        return abort(401, description="Sorry this venue already exists")
+
+    venue = Venue()
+
+    venue.venue_name = venue_fields["venue_name"]
+    venue.location = venue_fields["location"]
+    venue.user_id = user.id
+
+    db.session.add(venue)
+    db.session.commit()
+
+    return jsonify(venue_schema.dump(venue))
+
+
+# Put method to allow users to update venue
+# method takes user identity and venue identity
+# user identity is to authenticate and ensure user is authorized to access venue
+# fields are edited via input of dictionary data
+# returns updated venue information as json object
+@venues.route("/update/<int:user_id>/<int:venue_id>", methods=["PUT"])
+@jwt_required()
+@error_handlers
+def update_venue(user_id, venue_id):
+    user = get_jwt_identity()
+    venue_fields = venue_schema.load(request.json)
+    user = db.get_or_404(User, user_id, description="Invalid user, please check id")
+
+    venue = db.get_or_404(Venue, venue_id, description="Invalid venue id, please check venue id")
 
     
 
-    return jsonify(venue_schema.dump(venue), show_schema.dump(upcoming_shows))
+    if user.id != venue.user_id:
+        return abort(401, description="Sorry you do not have access to this venue")
+    
+    venue.venue_name = venue_fields["venue_name"]
+    venue.location = venue_fields["location"]
+    venue.user_id = user.id
+
+    db.session.commit()
+
+    return jsonify(venue_schema.dump(venue))
