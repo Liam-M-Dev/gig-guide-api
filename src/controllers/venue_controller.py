4935897694afcdaff06dfecd8a58a92fb4f19864
Liam-M-Dev <liammassey21@gmail.com>
2023-03-11 18:49:37 +1100

    Create route to get single venue with upcoming shows

--- src/controllers/venue_controller.py
+++ src/controllers/venue_controller.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify, request, abort
 from main import db
 from models.venue import Venue
-from schemas.venue_schema import venue_schema, venues_schema
+from models.show import Show
+from schemas.venue_schema import venue_schema, venues_schema, VenueSchema
+from schemas.show_schema import show_schema, ShowSchema
 
 
 
@@ -19,3 +21,22 @@ def get_bands():
     result = venues_schema.dump(venues_list)
 
     return jsonify(result)
+
+
+# Get method to access one venue through id,
+# Method takes venue id
+# returns json object of venue data including upcoming shows
+@venues.route("/display/venue/<int:id>", methods=["GET"])
+def display_venue(id):
+
+    venue = db.get_or_404(Venue, id, description="Invalid venue id, please check ID")
+
+    venue_schema = VenueSchema(only=["id", "venue_name", "location"])
+
+    upcoming_shows = db.get_or_404(Show, venue.id, description="There are no upcoming shows")
+
+    show_schema = ShowSchema(only=["show_name", "date"])
+
+    
+
+    return jsonify(venue_schema.dump(venue), show_schema.dump(upcoming_shows))
