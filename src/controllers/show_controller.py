737969eee4aa906e62afdba0085593c14f080372
Liam-M-Dev <liammassey21@gmail.com>
2023-03-16 13:42:02 +1100

    Modify show creation route to one controller using requests

--- src/controllers/show_controller.py
+++ src/controllers/show_controller.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, jsonify, request, abort
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from decorators.error_decorator import error_handlers
+from decorators.user_login import get_user_fromdb
 from main import db
 from models.band import Band
 from models.show import Show
 from models.venue import Venue
-from models.user import User
 from schemas.show_schema import show_schema, shows_schema, ShowSchema
 from schemas.venue_schema import VenueSchema
 
@@ -64,63 +64,45 @@ def search_shows():
 # method takes data input via fields for show name, date, band_id
 # sets shows venue id to the venue id
 # returns json object of show data
-@shows.route("/create/show/venue/<int:id>", methods=["POST"])
-@jwt_required()
+@shows.route("/create/show/", methods=["POST"])
 @error_handlers
-def show_creation_venue(id):
+@get_user_fromdb
+def show_creation(**kwargs):
 
-    venue = get_jwt_identity()
-
-    venue = db.get_or_404(Venue, id, description="Venue does not exist")
+    user = kwargs["user"]
 
     show_fields = show_schema.load(request.json)
-
-    show = Show.query.filter_by(show_name=show_fields["show_name"]).first()
-    if show:
-        return abort(401, description="Sorry this show already exists")
-
+    
     show = Show()
 
-    show.show_name = show_fields["show_name"]
-    show.date = show_fields["date"]
-    show.band_id = show_fields["band_id"]
-    show.venue_id = venue.id
+    if request.args.get("venue"):
+        venue = Venue.query.filter_by(id=request.args.get("venue")).first_or_404(description="Sorry this venue does not exist, please check id")
+        
+        if venue.user_id != user.id:
+            return jsonify({"message": "Sorry you do not have access to this venue to create a show"}), 401
 
+        show.show_name = show_fields["show_name"]
+        show.date = show_fields["date"]
+        show.band_id = show_fields["band_id"]
+        show.venue_id = venue.id
+    elif request.args.get("band"):
+        band = Band.query.filter_by(id=request.args.get("band")).first_or_404(description="Sorry this band does not exist, please check id")
+
+        if band.user_id != user.id:
+            return jsonify({"message": "Sorry you do not have access to this band to create a show"}), 401
+        
+        show.show_name = show_fields["show_name"]
+        show.date = show_fields["date"]
+        show.band_id = band.id
+        show.venue_id = show_fields["venue_id"]
+
+    show_exists = Show.query.filter_by(show_name=show_fields["show_name"]).first()
+    if show_exists:
+        return abort(401, description="Sorry this show already exists")
+    
     db.session.add(show)
     db.session.commit()
 
-    return jsonify(show_schema.dump(show))
-
-
-# Post method to allow user to create a new show
-# method requires venue id to authenticate venue
-# method takes data input via fields for show name, date, band_id
-# sets shows venue id to the venue id
-# returns json object of show data
-@shows.route("/create/show/band/<int:id>", methods=["POST"])
-@jwt_required()
-@error_handlers
-def show_creation_band(id):
-
-    band = get_jwt_identity()
-
-    band = db.get_or_404(Band, id, description="Band does not exist")
-
-    show_fields = show_schema.load(request.json)
-
-    show = Show.query.filter_by(show_name=show_fields["show_name"]).first()
-    if show:
-        return abort(401, description="Sorry this show already exists")
-
-    show = Show()
-
-    show.show_name = show_fields["show_name"]
-    show.date = show_fields["date"]
-    show.band_id = band.id
-    show.venue_id = show_fields["venue_id"]
-
-    db.session.add(show)
-    db.session.commit()
 
     return jsonify(show_schema.dump(show))
 
