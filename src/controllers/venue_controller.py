abcf4ea54a5e9686d6864e0ffb1ad9d35189299c
Liam-M-Dev <liammassey21@gmail.com>
2023-03-17 20:17:45 +1100

    Clean venue controller, add comments

--- src/controllers/venue_controller.py
+++ src/controllers/venue_controller.py
@@ -1,24 +1,32 @@
-from flask import Blueprint, jsonify, request, abort
-from decorators.error_decorator import error_handlers
-from decorators.venue_decorator import get_venue_fromdb
-from decorators.user_login import get_user_fromdb
-from main import db
-from models.venue import Venue
-from models.show import Show
-from schemas.venue_schema import venue_schema, venues_schema, VenueSchema
-from schemas.show_schema import ShowSchema
+try:
+    from flask import Blueprint, jsonify, request, abort
 
+    from decorators.error_decorator import error_handlers
+    from decorators.user_login import get_user_fromdb
+    from decorators.venue_decorator import get_venue_fromdb
+
+    from main import db
+
+    from models.show import Show
+    from models.venue import Venue
+
+    from schemas.show_schema import ShowSchema
+    from schemas.venue_schema import venue_schema, venues_schema, \
+        VenueSchema
+except ImportError:
+    print("Error has occurred with imports"
+          "Please check importing from modules is correct")
 
 
 venues = Blueprint("venues", __name__, url_prefix="/venues")
 
 
-# Get method for accessing all users and shows attendance,
-# will implement Authorization in a bit and change attendance 
-# from being viewable for admin
+# Get method for accessing all venues
+# returns all venues from venue table in JSON format
 @venues.route("/", methods=["GET"])
 @error_handlers
 def get_venues():
+    """Returns all venues from venue table"""
 
     venues_list = Venue.query.all()
 
@@ -27,41 +35,55 @@ def get_venues():
     return jsonify(result)
 
 
-# Get method to access one venue through id,
-# Method takes venue id
+# Get method to display one venue and its upcoming shows,
+# queries venue using route request of venue id integer
+# queries shows that has the venue id
 # returns json object of venue data including upcoming shows
 @venues.route("/display/venue/<int:id>", methods=["GET"])
 @error_handlers
 def display_venue(id):
-
-    venue = db.get_or_404(Venue, id, description="Invalid venue id, please check ID")
+    """Returns venue and upcoming shows in JSON format
+    
+    queries venue with request from route header
+    queries shows using venue.id
+    serializes venue and show objects through schemas
+    """
+    venue = db.get_or_404(Venue, \
+                          id, \
+                          description=\
+                            "Invalid venue id, please check ID")
 
     venue_display = VenueSchema(only=["id", "venue_name", "location"])
 
-    upcoming_shows = db.get_or_404(Show, venue.id, description="There are no upcoming shows")
+    upcoming_shows = db.get_or_404(Show, \
+                                   venue.id, \
+                                    description=\
+                                        "There are no upcoming shows")
 
     show_display = ShowSchema(only=["show_name", "date"])
 
-    return jsonify(venue_display.dump(venue), show_display.dump(upcoming_shows))
+    return jsonify(venue_display.dump(venue), \
+                   show_display.dump(upcoming_shows))
 
 
 # Post method to allow user to create a new venue
-# method requires user id to authenticate user
-# method takes data input via fields for venue name, location
-# sets venue user id to the users id
-# returns json object of venue data
+# gets validated user from user_from_db
+# takes input using fields to associate with venue attributes
+# returns registered venue in JSON format
 @venues.route("/register/", methods=["POST"])
 @error_handlers
 @get_user_fromdb
 def venue_creation(**kwargs):
+    """Creates venue in venue table"""
 
     user = kwargs["user"]
 
     venue_fields = venue_schema.load(request.json)
 
-    venue = Venue.query.filter_by(venue_name=venue_fields["venue_name"]).first()
+    venue = Venue.query\
+        .filter_by(venue_name=venue_fields["venue_name"]).first()
     if venue:
-        return abort(401, description="Sorry this venue already exists")
+        return jsonify({"message" : "Sorry venue already exists"}), 401
 
     venue = Venue()
 
@@ -76,16 +98,22 @@ def venue_creation(**kwargs):
 
 
 # Put method to allow users to update venue
-# method takes user identity and venue identity
-# user identity is to authenticate and ensure user is authorized to access venue
-# fields are edited via input of dictionary data
-# returns updated venue information as json object
+# gets validated user from get_user_fromdb
+# gets validated venue from get_venue_fromdb
+# checks that venue.user_id and user.id match
+# if not, returns error with JSON message and error code
+# else, takes input via venue fields to match with attributes
+# returns updated venue information in JSON format
 @venues.route("/update/<int:venue_id>", methods=["PUT"])
 @error_handlers
 @get_user_fromdb
 @get_venue_fromdb
 def update_venue(**kwargs):
-
+    """Updates venue object and commits to db
+    
+    method uses venue_fields to associate with venue attributes
+    """
+    
     user = kwargs["user"]
     venue = kwargs["venue"]
 
@@ -93,7 +121,9 @@ def update_venue(**kwargs):
     
 
     if user.id != venue.user_id:
-        return abort(401, description="Sorry you do not have access to this venue")
+        return jsonify({"message" : \
+                        "You do not have access to this venue"}), \
+                            401
     
     venue.venue_name = venue_fields["venue_name"]
     venue.location = venue_fields["location"]
@@ -105,20 +135,27 @@ def update_venue(**kwargs):
 
 
 # Delete route to allow a user to delete their venue
-# requires user id and venue id plus jwt authentication
-# checks that user owns venue before deletion or user is an admin
-# deletes venue and returns json format of message "venue deleted"
+# gets validated user from get_user_fromdb
+# gets validated venue from get_venue_fromdb
+# checks if venue.user_id matches user.id
+# if not returns error with JSON message and error code
+# else, deletes venue from venue table
+# returns message venue deleted
 @venues.route("/delete/<int:venue_id>", methods=["DELETE"])
 @error_handlers
 @get_user_fromdb
 @get_venue_fromdb
 def delete_venue(**kwargs):
+    """Deletes venue from venue table"""
+
 
     user = kwargs["user"]
     venue = kwargs["venue"]
 
     if user.id != venue.user_id and not user.admin:
-        return abort(401, description="Sorry you do not have access to this venue")
+        return jsonify({"message" : \
+                        "You do not have access to this venue"}), \
+                            401
     elif user.admin and not user.id != venue.user_id:
         db.session.delete(venue)
         db.session.commit()
@@ -126,4 +163,4 @@ def delete_venue(**kwargs):
         db.session.delete(venue)
         db.session.commit()
 
-    return jsonify({"msg": "venue deleted"})
+    return jsonify({"msg": "venue deleted"}), 200
